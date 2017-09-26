7b41b56015bfd0690c58f54124d46ce6639c296f
Kraig Brockschmidt <kraigbr@msn.com>
2017-09-26 16:07:44 -0700

    Added comments

--- CosineWave/CosineWave.py
+++ CosineWave/CosineWave.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from math import radians
 
 def main():  
+    # a np range works with plot
     x = np.arange(0, radians(1800), radians(12))
     plt.plot(x, np.cos(x), 'b')
     plt.show()
